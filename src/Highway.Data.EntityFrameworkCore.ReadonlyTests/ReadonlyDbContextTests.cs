16b6a16f8c3e36ab14fa5dcd02535d0d93563d79
Eric Burcham <Eric Burcham>
2022-02-09 14:18:16 -0600

    add solution folders

---
+++ src/Highway.Data.EntityFrameworkCore.ReadonlyTests/ReadonlyDbContextTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Highway.Data.EntityFrameworkCore.ReadonlyTests
+{
+    [TestClass]
+    public class ReadonlyDbContextTests
+    {
+        private static ReadonlyDbContext _target;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            _target = new ReadonlyDbContext(Configuration.Instance.TestDatabaseConnectionString, new NoOpLogger());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void CannotCallGenericSet()
+        {
+            var students = _target.Set<Student>();
+            students.Add(new Student());
+            _target.SaveChanges();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void CannotCallSaveChanges()
+        {
+            _target.SaveChanges();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public async Task CannotCallSaveChangesAsync()
+        {
+            await _target.SaveChangesAsync();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public async Task CannotCallSaveChangesAsyncWithCancellationToken()
+        {
+            await _target.SaveChangesAsync(new CancellationToken());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void CannotCallTypedSet()
+        {
+            var students = _target.Set(typeof(Student));
+            students.Add(new Student());
+            _target.SaveChanges();
+        }
+    }
+}
